f0ecb6a57ae71c02d91cc6037a3f8fef86ed887d
Dista Reza <dista.reza@bertelsmann.de>
2022-09-01 07:29:14 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-', ',':'--..--',
+                    ' ':' ' }
+
+
+def morse_generator(text):
+    encoded = ""
+    for char in text:
+        encoded += MORSE_CODE_DICT[char] + ' '
+    return encoded
+
+
+def print_hi(name):
+    message = f'Hi, {name}'
+    print(f"{message}")
+    morse_code = morse_generator(message.upper())
+    print(f"Encoded morse code : {morse_code}")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
